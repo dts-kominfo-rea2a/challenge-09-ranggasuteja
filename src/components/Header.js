715acc48d7171eec7f769311be60e38565fefd3c
ranggaSuteja <shuteja@gmail.com>
2022-06-21 21:32:33 +0700

    test2

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,15 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
-import React from 'react';
+import React from "react";
+import "./Style.css";
 
 const Header = () => {
-    return (
-        <></>
-    )
-}
+  return (
+    <div>
+      <h1>CALL A FRIEND</h1>
+      <p>Your Friendly Contact Apps</p>
+      <h2>*************************************************************</h2>
+    </div>
+  );
+};
 
 export default Header;
